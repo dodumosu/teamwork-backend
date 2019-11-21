9f48dab9f4f8ed6d5076f39d087e77402ddf7156
Odumosu Dipo <ironkyo@gmail.com>
2019-11-21 11:25:21 +0100

    feat: protect API routes

--- src/routes/article.js
+++ src/routes/article.js
@@ -1,13 +1,14 @@
 const { Router } = require('express');
 const articleController = require('../controllers/article');
+const authMiddleware = require('../middleware/auth');
 
 const router = Router();
 
-router.post('/articles', articleController.createArticle);
-router.patch('/articles/:articleId', articleController.updateArticle);
-router.delete('/articles/:articleId', articleController.deleteArticle);
-router.post('/articles/:articleId/comment', articleController.addArticleComment);
-router.get('/articles/:articleId', articleController.viewArticle);
-router.get('/articles', articleController.viewArticles);
+router.post('/articles', authMiddleware, articleController.createArticle);
+router.patch('/articles/:articleId', authMiddleware, articleController.updateArticle);
+router.delete('/articles/:articleId', authMiddleware, articleController.deleteArticle);
+router.post('/articles/:articleId/comment', authMiddleware, articleController.addArticleComment);
+router.get('/articles/:articleId', authMiddleware, articleController.viewArticle);
+router.get('/articles', authMiddleware, articleController.viewArticles);
 
 module.exports = router;
--- src/routes/gif.js
+++ src/routes/gif.js
@@ -1,12 +1,13 @@
 const { Router } = require('express');
+const authMiddleware = require('../middleware/auth');
 const gifController = require('../controllers/gif');
 
 const router = Router();
 
-router.post('/gifs', gifController.createGIF);
-router.delete('/gifs/:gifId', gifController.deleteGIF);
-router.post('/gifs/:gifId/comment', gifController.addGIFComment);
-router.get('/gifs/:gifId', gifController.viewGIF);
-router.get('/gifs', gifController.viewGIFs);
+router.post('/gifs', authMiddleware, gifController.createGIF);
+router.delete('/gifs/:gifId', authMiddleware, gifController.deleteGIF);
+router.post('/gifs/:gifId/comment', authMiddleware, gifController.addGIFComment);
+router.get('/gifs/:gifId', authMiddleware, gifController.viewGIF);
+router.get('/gifs', authMiddleware, gifController.viewGIFs);
 
 module.exports = router;
