7a0772a744db6bb26c26660b230135e262a0218b
Odumosu Dipo <ironkyo@gmail.com>
2019-11-21 11:25:48 +0100

    feat: add API routes to app

--- src/app.js
+++ src/app.js
@@ -3,14 +3,13 @@ const cookieParser = require('cookie-parser');
 const express = require('express');
 
 require('./config');
+const userRoutes = require('./routes/user');
+const articleRoutes = require('./routes/article');
+const gifRoutes = require('./routes/gif');
 
 const app = express();
 app.use(bodyParser.json());
-app.use(cookieParser());
-
-app.get('/', (request, response) => {
-  response.setHeader('Content-Type', 'application/json');
-  response.json({ message: 'Hello, world, from Express!' });
-});
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use('/api/v1', [articleRoutes, gifRoutes, userRoutes]);
 
 module.exports = app;
