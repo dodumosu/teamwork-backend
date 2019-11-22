39bd47641420ad062872298f53c9a976486fd3d6
Odumosu Dipo <ironkyo@gmail.com>
2019-11-22 11:21:53 +0100

    feat: use cors and helmet packages in app

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const helmet = require('helmet');
 const express = require('express');
 
 require('./config');
@@ -10,6 +12,8 @@ const gifRoutes = require('./routes/gif');
 const app = express();
 app.use(bodyParser.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(cors());
+app.use(helmet());
 app.use('/api/v1', [articleRoutes, gifRoutes, userRoutes]);
 
 module.exports = app;
