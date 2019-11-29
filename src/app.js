612ca8d85c55787332ec227c0bef85cc8c01895e
Odumosu Dipo <ironkyo@gmail.com>
2019-11-29 08:40:10 +0100

    fix: remove references to missing modules

--- src/app.js
+++ src/app.js
@@ -6,14 +6,13 @@ const express = require('express');
 
 require('./config');
 const userRoutes = require('./routes/user');
-const articleRoutes = require('./routes/article');
-const gifRoutes = require('./routes/gif');
+const postRoutes = require('./routes/post');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(cors());
 app.use(helmet());
-app.use('/api/v1', [articleRoutes, gifRoutes, userRoutes]);
+app.use('/api/v1', [postRoutes, userRoutes]);
 
 module.exports = app;
